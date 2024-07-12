398cef5c93e924c76af5167e165dedb316e422cd
AntonioviniciusA <antoniovinicius_@outlook.com>
2024-07-12 17:43:55 -0300

    3

--- JS/scrollline.js
+++ JS/scrollline.js
@@ -1,14 +1,6 @@
-        let lastScrollTop = 0;
-        let widthPercentage = 0;
-        window.addEventListener('scroll', function() {
-            const scrollTop = window.scrollY;
-            const scrollDiff = scrollTop - lastScrollTop;
-            lastScrollTop = scrollTop;
-            if (scrollDiff >= 100) {
-                widthPercentage += 10;
-                if (widthPercentage > 100) {
-                    widthPercentage = 100; 
-                }
-                document.getElementById('lineheader').style.width = widthPercentage + '%';
-            }
-        });
+window.addEventListener('scroll', function() {
+    const scrollTop = window.scrollY;
+    const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrollPercent = (scrollTop / docHeight) * 100;
+    document.getElementById('lineheader').style.width = scrollPercent + '%';
+  });
