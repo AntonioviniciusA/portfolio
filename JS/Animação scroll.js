ab06964abca575ecdfe5e3c2104567f6dabcfb64
AntonioviniciusA <antoniovinicius_@outlook.com>
2024-07-23 18:52:57 -0300

    3

--- JS/Animação scroll.js
+++ JS/Animação scroll.js
@@ -1,60 +1,56 @@
 const observador = new IntersectionObserver((entries) => {
-  entries.forEach( (entry) => {
-    if(entry.isIntersecting){
-        entry.target.classList.add('visivel')
-    } else{
-        entry.target.classList.remove('visivel')
-        }
-    })
-})
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visivel");
+    } else {
+      entry.target.classList.remove("visivel");
+    }
+  });
+});
 
-const elements = document.querySelectorAll('.escondido')
+const elements = document.querySelectorAll(".escondido");
 
-elements.forEach((element) => observador.observe(element))
+elements.forEach((element) => observador.observe(element));
 
-
-/*tri*/ 
+/*tri*/
 const obv = new IntersectionObserver((entries) => {
-    entries.forEach( (entry) => {
-      if(entry.isIntersecting){
-          entry.target.classList.add('visivel-tri')
-      } else{
-          entry.target.classList.remove('visivel-tri')
-          }
-      })
-  })
-  
-  const elementos = document.querySelectorAll('.escondido-tri')
-  
-  elementos.forEach((element) => obv.observe(element))
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visivel-tri");
+    } else {
+      entry.target.classList.remove("visivel-tri");
+    }
+  });
+});
 
-  const obvs = new IntersectionObserver((entries) => {
-    entries.forEach( (entry) => {
-      if(entry.isIntersecting){
-          entry.target.classList.add('visivel-tri2')
-      } else{
-          entry.target.classList.remove('visivel-tri2')
-          }
-      })
-  })
-  
-  const element = document.querySelectorAll('.escondido-tri2')
-  
-  element.forEach((element) => obv.observe(element))
+const elementos = document.querySelectorAll(".escondido-tri");
 
+elementos.forEach((element) => obv.observe(element));
 
+const obvs = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visivel-tri2");
+    } else {
+      entry.target.classList.remove("visivel-tri2");
+    }
+  });
+});
 
-  const obvss = new IntersectionObserver((entries) => {
-    entries.forEach( (entry) => {
-      if(entry.isIntersecting){
-          entry.target.classList.add('visivel-tri3')
-      } else{
-          entry.target.classList.remove('visivel-tri3')
-          }
-      })
-  })
-  
-  const eleme = document.querySelectorAll('.escondido-tri3')
-  
-  eleme.forEach((element) => obv.observe(element))
-  
+const element = document.querySelectorAll(".escondido-tri2");
+
+element.forEach((element) => obv.observe(element));
+
+const obvss = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visivel-tri3");
+    } else {
+      entry.target.classList.remove("visivel-tri3");
+    }
+  });
+});
+
+const eleme = document.querySelectorAll(".escondido-tri3");
+
+eleme.forEach((element) => obv.observe(element));
