32dd23570207063eb8bac4ed91fa5ed1f5e78a2b
AntonioviniciusA <antonio.alvino@a.ucb.br>
2025-04-24 03:17:30 -0300

    Refactor CSS styles for scrollbar and layout; update loading animation logic to hide loading screen after 5 seconds.

--- JS/LoadingAnimation.js
+++ JS/LoadingAnimation.js
@@ -2,10 +2,14 @@
 window.addEventListener("load", () => {
   const loadingScreen = document.getElementById("loading-screen");
   const mainContent = document.getElementById("main-content");
-
-  // Esconde a tela de loading após 3 segundos
+  const footer = document.getElementById("footer");
+  const body = document.body;
+  setTimeout(() => {
+    mainContent.style.display = "block";
+    footer.style.display = "flex";
+    body.style.backgroundColor = "transparent"; // Permite o scroll do body
+  }, 3000); // Ajuste o tempo de exibição do loading conforme necessário
   setTimeout(() => {
     loadingScreen.style.display = "none";
-    mainContent.style.display = "block";
   }, 5000); // Ajuste o tempo de exibição do loading conforme necessário
 });
